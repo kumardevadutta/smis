d1b95fa1ec2004bf731c5005f3fc16b63d8b08b0
jay2452 <jay2452@live.com>
2016-06-12 18:21:28 +0530

    kiit

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
 	has_one :student_mark, dependent: :destroy
 	validates :roll, presence: true
-	validates :sname, presence: true
+	validates :sname, presence: true 
 end
